0689bdcedfdce0a0c15580d40dafc09da4e4bb4e
Sven-Ole Bittner <sven-ole.bittner@gmx.de>
2025-03-02 16:43:15 +0100

    implemnt the init() and include X / O function

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ let fields = [
     null,
 ];
 
+let currentPlayer = 'X';
+
+function init() {
+    render();
+}
+
 function render() {
     const gameContainer = document.getElementById('gameContainer');
     let tableHTML = '<table style="border-collapse: collapse;">';
@@ -19,13 +25,21 @@ function render() {
         for (let j = 0; j < 3; j++) {
             const cellIndex = i * 3 + j;
             const cellContent = fields[cellIndex] === null ? '' : fields[cellIndex];
-            tableHTML += `<td style="width: 100px; height: 100px; border: 1px solid black; text-align: center; vertical-align: middle; font-size: 24px;">${cellContent}</td>`;
+            tableHTML += `<td onclick="makeMove(${cellIndex})" style="width: 100px; height: 100px; border: 1px solid black; text-align: center; vertical-align: middle; font-size: 24px; cursor: pointer;">${cellContent}</td>`;
         }
         tableHTML += '</tr>';
     }
     tableHTML += '</table>';
 
     gameContainer.innerHTML = tableHTML;
+}
+
+function makeMove(index) {
+    if (fields[index] === null) {
+        fields[index] = currentPlayer;
+        currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        render();
+    }
 }
 
 render();
